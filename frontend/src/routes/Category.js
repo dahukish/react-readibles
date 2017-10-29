fc25a661995bf907a0fe9f18cbbc7772df75a034
Stephen Hukish <stephen.hukish@gmail.com>
2017-10-29 12:40:25 -0400

    feat(frontend): update category route to reflect changes on home route

--- frontend/src/routes/Category.js
+++ frontend/src/routes/Category.js
@@ -2,85 +2,120 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Card, CardActions, CardHeader, CardText } from 'material-ui/Card';
-import ActionThumbUp from 'material-ui/svg-icons/action/thumb-up';
-import ActionThumbDown from 'material-ui/svg-icons/action/thumb-down';
-import ActionDelete from 'material-ui/svg-icons/action/delete';
 import FlatButton from 'material-ui/FlatButton';
-import FloatingActionButton from 'material-ui/FloatingActionButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
+import Toggle from 'material-ui/Toggle';
+import Dialog from 'material-ui/Dialog';
+import uuidv4 from 'uuid/v4';
 import * as Actions from '../store/actions';
-import { getPostsForCategorySorted } from '../selectors';
-
-const onclick = (buttonName) => {
-    console.log(`${buttonName} be clicked dude!`);
-};
+import {
+    getApplicationSate
+} from '../selectors';
+import PostList from '../components/PostList';
+import PostForm from '../forms/post';
 
 const Category = (props) => {
     return (
         <div className="posts-list">
             <Toolbar>
                 <ToolbarGroup firstChild={true}>
-                    <RaisedButton label="Go Back" primary={true} onClick={() => {
-                        props.actions.push('/');
-                    }} />
+                    <RaisedButton
+                        label='Back'
+                        onClick={() => {
+                            props.actions.push('/');
+                        }} />
+                </ToolbarGroup>
+                <ToolbarGroup>
+                    <Toggle
+                        label="Sort By Date (Default is Vote Score)"
+                        defaultToggled={false}
+                        onToggle={() => {
+                            const newToggleState = !props.ui.togglePostTypeUIState;
+                            const newSortType = (newToggleState === true) ? 'timestamp' : 'voteScore';
+                            props.actions.togglePostSortType(newToggleState);
+                            props.actions.changePostsSort(newSortType, props.ui.sortPostOrder);
+                        }}
+                        labelPosition="right"
+                        style={{ margin: 20 }}
+                    />
+                    <Toggle
+                        label="Sort Asc (Default is Desc)"
+                        defaultToggled={true}
+                        onToggle={() => {
+                            const newToggleState = !props.ui.togglePostOrderUIState;
+                            const newOrder = (newToggleState === true) ? 'asc' : 'desc';
+                            props.actions.togglePostSortOrder(newToggleState);
+                            props.actions.changePostsSort(props.ui.sortPostType, newOrder);
+                        }}
+                        labelPosition="right"
+                        style={{ margin: 20 }}
+                    />
                 </ToolbarGroup>
             </Toolbar>
-            {
-                props.posts.map((post) => (
-                    <Card key={post.id}>
-                        <CardHeader
-                            title={post.title}
-                            subtitle={post.author}
-                            actAsExpander={true}
-                            showExpandableButton={true}
-                        />
-                        <div>Vote Score: {post.voteScore}</div>
-                        <div>Category: {post.category}</div>
-                        <div>Comments: {post.commentCount}</div>
-                        <CardActions>
-                            <FloatingActionButton mini={true} onClick={() => {
-                                props.actions.voteForPost(post.id, 'upVote');
-                            }}>
-                                <ActionThumbUp />
-                            </FloatingActionButton>
-                            <FloatingActionButton mini={true} onClick={() => {
-                                props.actions.voteForPost(post.id, 'downVote');
-                            }}>
-                                <ActionThumbDown />
-                            </FloatingActionButton>
-                            <FlatButton label="View Post" onClick={() => {
-                                onclick(`view post for ${post.id}`);
-                            }} />
-                            <FlatButton label="Edit Post" onClick={() => {
-                                onclick(`view post for ${post.id}`);
-                            }} />
-                            <FloatingActionButton mini={true} onClick={() => {
-                                onclick('delete');
-                            }}>
-                                <ActionDelete />
-                            </FloatingActionButton>
-                        </CardActions>
-                        <CardText expandable={true}>
-                            TODO: COMMENTS GO HERE
-                </CardText>
-                    </Card>
-                ))
-            }
+            <PostList match={props.match} />
+            <Dialog
+                open={props.ui.modals.post}
+                actions={[
+                    <FlatButton
+                        key={'cancelButton'}
+                        label="Cancel"
+                        primary={true}
+                        onClick={() => {
+                            props.actions.resetPostForm();
+                            props.actions.toggleModalState('post', false);
+                        }}
+                    />,
+                    <FlatButton
+                        key={'submitButton'}
+                        label="Submit"
+                        primary={true}
+                        onClick={() => {
+                            props.actions.submitPostForm();
+                            props.actions.toggleModalState('post', false);
+                        }}
+                    />,
+                ]}
+            >
+                <PostForm onSubmit={(values) => {
+                    if (props.ui.postFormMode === 'create') {
+                        const newPost = {
+                            id: uuidv4(),
+                            timestamp: Date.now(),
+                            voteScore: 1,
+                            ...values
+                        };
+                        props.actions.submitNewPostValues(newPost).then(() => {
+                            props.actions.setCurrentPost(null);
+                            props.actions.toggleModalState('post', false);
+                        });
+                    } else {
+                        const updatedPost = {
+                            timestamp: Date.now(),
+                            ...values
+                        };
+                        props.actions.submitUpdatedPostValues(values.id, updatedPost).then(() => {
+                            props.actions.setCurrentPost(null);
+                            props.actions.toggleModalState('post', false);
+                        });
+                    }
+
+                }} />
+            </Dialog>
         </div>
     );
 };
 
 Category.propTypes = {
+    match: PropTypes.object.isRequired,
+    ui: PropTypes.object.isRequired,
     actions: PropTypes.object.isRequired,
     history: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state, props) => {
     return {
-        posts: getPostsForCategorySorted(state, props),
-        categories: state.categoriesReducer.categories
+        ui: getApplicationSate(state)
     };
 };
 
