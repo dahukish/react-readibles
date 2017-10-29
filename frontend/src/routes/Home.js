104cbba9e590e70af042e568c41339ac88c1a850
Stephen Hukish <stephen.hukish@gmail.com>
2017-10-29 12:41:12 -0400

    feat(frontend):
    - update home route to use new child components

--- frontend/src/routes/Home.js
+++ frontend/src/routes/Home.js
@@ -2,33 +2,16 @@ import React from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { Card, CardActions, CardHeader } from 'material-ui/Card';
 import { Toolbar, ToolbarGroup } from 'material-ui/Toolbar';
 import Toggle from 'material-ui/Toggle';
-import RaisedButton from 'material-ui/RaisedButton';
-import ActionThumbUp from 'material-ui/svg-icons/action/thumb-up';
-import ActionThumbDown from 'material-ui/svg-icons/action/thumb-down';
 import ContentAdd from 'material-ui/svg-icons/content/add';
-import ActionDelete from 'material-ui/svg-icons/action/delete';
-import FlatButton from 'material-ui/FlatButton';
 import FloatingActionButton from 'material-ui/FloatingActionButton';
-import Dialog from 'material-ui/Dialog';
-import uuidv4 from 'uuid/v4';
 import * as Actions from '../store/actions';
 import {
-    getPostsSorted,
-    getCategories,
-    getUIState
+    getApplicationSate
 } from '../selectors';
-import PostForm from '../forms/post';
-
-const getCategoryPath = (categories, currentCat) => {
-    let categoryPath;
-    if (categories.length) {
-        categoryPath = categories.filter(category => category.name === currentCat)[0].path
-    }
-    return categoryPath || '';
-};
+import PostList from '../components/PostList';
+import PostActions from '../components/PostActions';
 
 const Home = (props) => {
     return (
@@ -60,126 +43,29 @@ const Home = (props) => {
                         style={{ margin: 20 }}
                     />
                 </ToolbarGroup>
-                <ToolbarGroup style={{zIndex: '2000'}}>
+                <ToolbarGroup style={{ zIndex: '2000' }}>
                     <FloatingActionButton mini={true} onClick={() => {
                         props.actions.setPostFormMode('create');
-                        props.actions.toggleModalState(true);
+                        props.actions.toggleModalState('post', true);
                     }}>
                         <ContentAdd />
                     </FloatingActionButton>
                 </ToolbarGroup>
             </Toolbar>
-            {
-                props.posts.map((post) => (
-                    <Card key={post.id}>
-                        <CardHeader
-                            title={post.title}
-                            subtitle={post.author}
-                        >
-                            <RaisedButton
-                                label={post.category}
-                                secondary={true}
-                                onClick={() => {
-                                    props.actions.push(`/${getCategoryPath(props.categories, post.category)}`);
-                                }} />
-                        </CardHeader>
-                        <div>Vote Score: {post.voteScore}</div>
-                        <div>Comments: {post.commentCount}</div>
-                        <CardActions>
-                            <FloatingActionButton mini={true} onClick={() => {
-                                props.actions.voteForPost(
-                                    post.id,
-                                    'upVote'
-                                );
-                            }}>
-                                <ActionThumbUp />
-                            </FloatingActionButton>
-                            <FloatingActionButton mini={true} onClick={() => {
-                                props.actions.voteForPost(
-                                    post.id,
-                                    'downVote'
-                                );
-                            }}>
-                                <ActionThumbDown />
-                            </FloatingActionButton>
-                            <FlatButton label="View Post" onClick={() => {
-                                props.actions.push(`/${getCategoryPath(props.categories, post.category)}/${post.id}`);
-                            }}>
-
-                            </FlatButton>
-                            <FlatButton label="Edit Post" onClick={() => {
-                                props.actions.setCurrentPost(post);
-                                props.actions.setPostFormMode('edit');
-                                props.actions.toggleModalState(true);
-                            }} />
-                            <FloatingActionButton mini={true} onClick={() => {
-                                props.actions.deletePost(post.id);
-                            }}>
-                                <ActionDelete />
-                            </FloatingActionButton>
-                        </CardActions>
-                    </Card>
-                ))
-            }
-            <Dialog
-                open={props.ui.modalOpen}
-                actions={[
-                    <FlatButton
-                        label="Cancel"
-                        primary={true}
-                        onClick={() => {
-                            props.actions.resetPostForm();
-                            props.actions.toggleModalState(false);
-                        }}
-                    />,
-                    <FlatButton
-                        label="Submit"
-                        primary={true}
-                        onClick={() => {
-                            props.actions.submitPostForm();
-                            props.actions.toggleModalState(false);
-                        }}
-                    />,
-                ]}
-            >
-                <PostForm onSubmit={(values) => {
-                    if (props.ui.postFormMode === 'create') {
-                        const newPost = {
-                            id: uuidv4(),
-                            timestamp: Date.now(),
-                            voteScore: 1,
-                            ...values
-                        };
-                        props.actions.submitNewPostValues(newPost).then(() => {
-                            props.actions.setCurrentPost(null);
-                            props.actions.toggleModalState(false);
-                        });
-                    } else {
-                        const updatedPost = {
-                            timestamp: Date.now(),
-                            ...values
-                        };
-                        props.actions.submitUpdatedPostValues(values.id, updatedPost).then(() => {
-                            props.actions.setCurrentPost(null);
-                            props.actions.toggleModalState(false);
-                        });
-                    }
-
-                }} />
-            </Dialog>
+            <PostList/>
+            <PostActions/>
         </div>
     );
 };
 
 Home.propTypes = {
+    ui: PropTypes.object.isRequired,
     actions: PropTypes.object.isRequired
 };
 
 const mapStateToProps = (state) => {
     return {
-        posts: getPostsSorted(state),
-        categories: getCategories(state),
-        ui: getUIState(state)
+        ui: getApplicationSate(state)
     };
 };
 
